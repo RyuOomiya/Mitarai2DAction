e09239c4e4499ffaf0c1848d5922d147c264709b
ryuoomiya <oomiyaryu@gmail.com>
2023-05-22 14:30:21 +0900

    [Add] desktop初コミット

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -6,12 +6,16 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject _player;
     [SerializeField] GameObject _bullet;
     [SerializeField] float _fireDis;
-    // Update is called once per frame
+
+    private void Awake()
+    {
+        _player = GameObject.Find("Player");
+    }
     void Update()
     {
         if(Vector3.Distance(_player.transform.position,transform.position) <= _fireDis)
         {
-            
+            StartCoroutine(Shoot());
         }
     }
 
@@ -19,7 +23,7 @@ public class Enemy : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
 
-        Instantiate(_bullet, new Vector2(transform.position.x, 1), Quaternion.identity).
+        Instantiate(_bullet, transform.position, Quaternion.identity).
             GetComponent<Bullet>().SetBulletDirection(-1);
     }
 }
